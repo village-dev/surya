f61ddce0631d85b1872ecb365edebde05f881865
Kevin Hu <kevinhuwest@gmail.com>
2025-01-09 14:19:17 -0800

    Fix cast

--- surya/ocr_error.py
+++ surya/ocr_error.py
@@ -39,7 +39,7 @@ def batch_ocr_error_detection(
 
         with torch.inference_mode():
             pred = model(batch_input_ids, attention_mask=batch_attention_mask)
-            logits = pred.logits.detach().cpu().numpy().astype(np.float32)
+            logits = pred.logits.detach().cpu().to(torch.float32).numpy()
             predictions.extend(np.argmax(logits, axis=1).tolist())
 
     return OCRErrorDetectionResult(
